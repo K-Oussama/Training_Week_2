608cfadcb020f1359ed51e6a7014d95a0cf87468
K-Oussama <oussama_korchi@um5.ac.ma>
2023-04-12 09:34:57 -0600

    injection via xml

--- Day 3 - Exercice 1/spring_exercice_1/src/main/java/com/example/spring_exercice_1/HelloWorldController.java
+++ Day 3 - Exercice 1/spring_exercice_1/src/main/java/com/example/spring_exercice_1/HelloWorldController.java
@@ -1,13 +1,11 @@
 package com.example.spring_exercice_1;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HelloWorldController {
     private HelloWorldService helloWorldService;
 
-    @Autowired
     public void setHelloWorldService(HelloWorldService helloWorldService) {
         this.helloWorldService = helloWorldService;
     }
